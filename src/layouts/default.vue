63a90f249a803561d5452116827f53775eeca23d
Yemi Kudaisi <yemikudaisi@gmail.com>
2024-04-13 13:40:30 +0100

    fixed: stick side bar redesign

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,47 +1,35 @@
 <template>
-  <div class="min-h-0 flex-1 flex overflow-hidden">
-    <nav
-      aria-label="Sidebar"
-      style="height: 100svh"
-      class="hidden lg:block flex-shrink-0 bg-gray-800 overflow-y-auto"
-    >
-      <div
-        style="width: 70px; height: 100svh"
-        class="sidebar relative flex space-y-16 flex-col p-3 items-stretch"
-      >
-        <div class="grid grid-cols-1 justify-items-center gap-2">
-          <button>
-            <Icon name="heroicons:home" class="sidebar-icon block" />
-          </button>
-          <button>
-            <Icon name="heroicons:squares-2x2" class="sidebar-icon block" />
-          </button>
-          <button>
-            <Icon name="heroicons:server-stack" class="sidebar-icon block" />
-          </button>
-          <button>
-            <Icon
-              name="heroicons:information-circle"
-              class="sidebar-icon block"
-            />
-          </button>
-          <button class="place-self-end">
-            <Icon
-              name="heroicons:adjustments-horizontal"
-              class="sidebar-icon block"
-            />
-          </button>
-        </div>
-      </div>
-    </nav>
-    <main class="main w-full p-4">
-      <ScrollAreaRoot class="ScrollAreaRoot" style="--scrollbar-size: 10px">
-        <ScrollAreaViewport class="ScrollAreaViewport">
-            <slot/>
-        </ScrollAreaViewPort>
-        </ScrollAreaRoot>
-    </main>
-  </div>
+<div class="sidebar">
+    <div class="grid grid-cols-1 justify-items-center gap-5">
+        <button class="sidebar-icon-btn">
+        <Icon name="heroicons:home" class="sidebar-icon block" />
+        </button>
+        <button class="sidebar-icon-btn">
+        <Icon name="heroicons:squares-2x2" class="sidebar-icon block" />
+        </button>
+        <button class="sidebar-icon-btn">
+        <Icon name="heroicons:server-stack" class="sidebar-icon block" />
+        </button class="sidebar-icon-btn">
+        <span class="h-full"></span>
+        <button class="sidebar-icon-btn absolute bottom-16">
+        <Icon
+            name="heroicons:information-circle"
+            class="sidebar-icon block"
+        />
+        </button>
+        <button class="sidebar-icon-btn absolute bottom-5">
+        <Icon
+            name="heroicons:adjustments-horizontal"
+            class="sidebar-icon block"
+        />
+        </button>
+    </div>
+</div>
+
+<div class="main">
+    <slot />
+</div>
+
 </template>
 <script></script>
 <style style lang="scss">
@@ -49,21 +37,61 @@
 @tailwind components;
 @tailwind utilities;
 
-$sidebar-text-color: #8a96a3;
+$main-bg: #1f2428;
+$sidebar-txt-color: #8a96a3;
+
+body {
+  font-family: "Lato", sans-serif;
+}
 
 .sidebar {
-  background: #24292e;
+  height: 100%;
+  width: 58px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+  background-color: #24292e;
   border-right: solid 1px #4a555f;
 }
 
-.main {
-  background: #1f2428;
-}
 
 .sidebar-icon {
   width: 30px;
   height: 30px;
-  color: $sidebar-text-color;
+}
+
+.sidebar-icon-btn{
+  color: $sidebar-txt-color;
+}
+
+.sidebar-icon-btn:hover{
+  color: white;
+}
+
+.sidebar a:hover {
+  color: #f1f1f1;
+}
+
+.main {
+  margin-left: 58px; /* 58px for sidebar */
+  font-size: 28px; /* Increased text to enable scrolling */
+  padding-left: 40px;
+  padding-right: 40px;
+  padding-top: 20px;
+  padding-bottom: 0;
+  background: $main-bg;
+}
+
+body{
+  background: $main-bg;
+}
+
+@media screen and (max-height: 450px) {
+  .sidebar {padding-top: 15px;}
+  .sidebar a {font-size: 18px;}
 }
 
 /*
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,38 @@
     <div class="flex-col">
         <h1 class="text-2xl text-white">Server Status</h1>
     </div>
-    <div class="columns-3">
-        <div class="w-full" style="height: 250px;"></div>
+    <div class="columns-3 gap-x-8 gap-y-16 mt-5">
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>        
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
+        <div class="w-full server-status-card" style="height: 150px;">jhhj</div>
     </div>
 </template>
+<style>
+
+.server-status-card{
+    background-color: #181C1F;
+    border: solid 1px #1E2022;
+    filter: drop-shadow(0 2px 1px rgb(0 0 0 / 0.10));
+    border-radius: 5px;
+    padding: 20px;
+    margin-bottom: 40px;
+}
+</style>
