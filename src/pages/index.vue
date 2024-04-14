dfcc66c66cc267f1cca103ebcb5c902959034946
Yemi Kudaisi <yemikudaisi@gmail.com>
2024-04-14 16:10:27 +0100

    created service status component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,18 +10,7 @@
       </div>
     </div>
     <div class="flex flex-wrap mt-5 h-full md:gap-20 gap-40">
-      <div v-for="item in home.items" class="server-status-card">
-        <div class="flex flex-col">
-          <div class="flex justify-between">
-            <div class="text-2xl pb-4 txt-color-1">{{ item.name }}</div>
-            <div>
-              <div class="status-indicator" :class="getStatusColor(item.status)"></div>
-            </div>
-          </div>
-          <div class="txt-color-2 leading-loose">Online, {{ item.statusDuration }}</div>
-          <div class="txt-color-2 leading-loose">Last checked {{ item.lastChecked }} ago</div>
-        </div>
-      </div>
+      <StatusCard v-for="item in home.items" :service="item" />
     </div>
   </div>
 </template>
@@ -39,16 +28,6 @@ const home: HomeState = reactive(
     items: db.services.overview,
   }
 )
-
-function getStatusColor(status: ServiceStatus) {
-  switch (status) {
-    case ServiceStatus.Offline:
-      return 'bg-danger'
-    case ServiceStatus.Online:
-      return 'bg-primary'
-  }
-
-}
 </script>
 
 <style lang="scss"></style>
